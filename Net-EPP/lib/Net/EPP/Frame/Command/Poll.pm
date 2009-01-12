3fddbc126d76f711e8caaed5f4628c81ff3e508a
Gavin Brown <gavin.brown@centralnic.com>
2009-01-12 12:23:16 +0000

    updated copyright dates on files, bumped version.

--- Net-EPP/lib/Net/EPP/Frame/Command/Poll.pm
+++ Net-EPP/lib/Net/EPP/Frame/Command/Poll.pm
@@ -1,4 +1,4 @@
-# Copyright (c) 2007 CentralNic Ltd. All rights reserved. This program is
+# Copyright (c) 2009 CentralNic Ltd. All rights reserved. This program is
 # free software; you can redistribute it and/or modify it under the same
 # terms as Perl itself.
 # 
--- Net-EPP/t/use.t
+++ Net-EPP/t/use.t
@@ -1,8 +1,9 @@
 #!/usr/bin/env perl -w
+# $Id$
 use strict;
 use Test;
 BEGIN { plan tests => 1 }
 
-use Net::EPP::Client; ok(1);
+use Net::EPP::Simple; ok(1);
 
 exit;
