85e9d6744b88a2d9f41774271a6f3741864536fb
Gavin Brown <gavin.brown@centralnic.com>
2015-09-28 13:16:30 +0100

    ignore non-element children

--- Net-EPP/lib/Net/EPP/Simple.pm
+++ Net-EPP/lib/Net/EPP/Simple.pm
@@ -941,6 +941,7 @@ sub _contact_infData_to_hash {
 		if ($addrs->size == 1) {
 			my $addr = $addrs->shift;
 			foreach my $child ($addr->childNodes) {
+				next if (XML_ELEMENT_NODE != $child->nodeType);
 				if ($child->localName eq 'street') {
 					push(@{$ref->{addr}->{$child->localName}}, $child->textContent);
 
